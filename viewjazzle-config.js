981361447ef6bb37c63a549a8377c442b0fb8897
Scott Donovan <scott.donovan@bookatable.com>
2015-10-26 16:01:20 +0000

    Added additional viewport sizes to the configuration example

--- viewjazzle-config.js
+++ viewjazzle-config.js
@@ -5,4 +5,4 @@ exports.jsLibraryPath = 'lib/jquery-1.11.1/jquery-1.11.1.min.js'; // loads any o
 exports.jasminePath = 'lib/jasmine-2.1.0/';
 exports.render = false; // render a test failure
 exports.timeout = 60000;
-exports.viewports = [{ width: 1400, height: 768 }, { width: 768, height: 1024 }, { width: 320, height: 640 }];
+exports.viewports = [{ width: 1400, height: 768 }, { width: 1024, height: 768 }, { width: 768, height: 1024 }, { width: 320, height: 640 }];
